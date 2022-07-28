bd989ad8b398be026f35202edb3daa978c1338c8
SerginhoLeal <sergiojunioleal@hotmail.com>
2022-07-28 12:08:44 -0300

    finishing the first step of the button

--- src/common/button/index.tsx
+++ src/common/button/index.tsx
@@ -1,8 +1,24 @@
-import React from 'react';
-import { View } from 'react-native';
+import * as React from 'react';
+import * as Native from 'react-native';
+import * as Styles from './styles';
 
-// import { Container } from './styles';
+import { RectButtonProps } from 'react-native-gesture-handler';
 
-export const Button: React.FC = () => {
-  return <View />;
+type Props = RectButtonProps & {
+  title: string;
+  type?: Styles.TypeProps;
+  isLoading?: boolean;
+}
+
+export const Button: React.FC<Props> = ({ 
+  title,
+  type = 'primary',
+  isLoading = false,
+  ...rest
+}: Props) => {
+  return (
+    <Styles.Container type={type} enabled={!isLoading} {...rest}>
+      {isLoading ? <Styles.Load /> : <Styles.Title>{title}</Styles.Title>}
+    </Styles.Container>
+  );
 };
--- src/common/button/styles.ts
+++ src/common/button/styles.ts
@@ -1,5 +1,34 @@
-import styled from 'styled-components/native';
+import styled, { css } from 'styled-components/native';
+import { RectButton } from 'react-native-gesture-handler';
 
-export const Container = styled.View`
-  
+export type TypeProps = 'primary' | 'secondary';
+
+type ContainerProps = {
+  type: TypeProps;
+};
+
+export const Container = styled(RectButton)<ContainerProps>`
+  flex: 1;
+  max-height: 56px;
+  min-height: 56px;
+
+  justify-content: center;
+  align-items: center;
+
+  ${({ theme, type }) => css`
+    border-radius: ${theme.settings.border_radius};
+    background-color: ${type === 'primary' ? theme.colors.primary_900 : theme.colors.primary_800};
+  `};
 `;
+
+export const Title = styled.Text`
+  ${({ theme }) => css`
+    font-size: ${theme.fonts.size.small};
+    font-family: ${theme.fonts.text};
+    color: ${theme.colors.title};
+  `}
+`;
+
+export const Load = styled.ActivityIndicator.attrs(({ theme }) => ({
+  color: theme.colors.title
+}))``;
