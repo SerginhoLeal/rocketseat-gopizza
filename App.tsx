0ed76ac83673cb2d562304f403a53c4363b17938
SerginhoLeal <sergiojunioleal@hotmail.com>
2022-07-28 11:26:44 -0300

    adding the screen signin

--- App.tsx
+++ App.tsx
@@ -1,4 +1,7 @@
 import * as React from 'react';
+import * as Native from 'react-native';
+
+export { StatusBar} from 'expo-status-bar';
 
 // AppLoading segura o splash deixando ela visivel até tudo carregar
 import AppLoading from 'expo-app-loading';
@@ -7,21 +10,26 @@ import { useFonts, DMSans_400Regular } from '@expo-google-fonts/dm-sans';
 import { DMSerifDisplay_400Regular } from '@expo-google-fonts/dm-serif-display';
 
 import { ThemeProvider } from "styled-components/native";
-import theme from '@src/theme';
+import theme from './src/theme';
+
+import Signin from './src/screens/signin';
 
 export default function App() {
-  const [ fontsLoaded ] = useFonts({
+  Native.LogBox.ignoreAllLogs(true);
+
+  const [ loaded ] = useFonts({
     DMSans_400Regular,
     DMSerifDisplay_400Regular,
   });
 
-  if(!fontsLoaded){
+  if(!loaded){
     return <AppLoading />
   };
 
   return (
     <ThemeProvider theme={theme}>
-
+      <Native.StatusBar backgroundColor="transparent" translucent barStyle="light-content" />
+      <Signin />
     </ThemeProvider>
   );
 };
