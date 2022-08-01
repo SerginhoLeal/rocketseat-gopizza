e5451595fc5ecb54bf9e9294ad023921c8ef4305
SerginhoLeal <sergiojunioleal@hotmail.com>
2022-07-31 23:02:19 -0300

    complete layout

--- src/screens/signin/index.tsx
+++ src/screens/signin/index.tsx
@@ -4,25 +4,40 @@ import * as Styles from './styles';
 
 import { Input, Button } from '@common';
 
+import BrandImg from '@assets/brand.png';
+
 const Signin: React.FC = () => {
   return (
     <Styles.Container>
-      <Input
-        placeholder='E-mail'
-        type='secondary'
-        autoCorrect={false}
-        autoCapitalize="none"
-      />
-      <Input
-        placeholder='Senha'
-        type='secondary'
-        secureTextEntry
-      />
-      <Button
-        title='Entrar'
-        type='secondary'
-        isLoading={false}
-      />
+      <Native.KeyboardAvoidingView behavior={Native.Platform.OS === 'ios' ? 'padding' : undefined}>
+        <Styles.Content>
+          <Styles.Brand source={BrandImg} />
+          <Styles.Title>Login</Styles.Title>
+          <Input
+            placeholder='E-mail'
+            type='secondary'
+            autoCorrect={false}
+            autoCapitalize="none"
+          />
+          <Input
+            placeholder='Senha'
+            type='secondary'
+            secureTextEntry
+          />
+
+          <Styles.ForgotPasswordButton>
+            <Styles.ForgotPasswordLabel>
+              Esqueci minha senha
+            </Styles.ForgotPasswordLabel>
+          </Styles.ForgotPasswordButton>
+
+          <Button
+            title='Entrar'
+            type='secondary'
+            isLoading={false}
+          />
+        </Styles.Content>
+      </Native.KeyboardAvoidingView>
     </Styles.Container>
   );
 }
--- src/screens/signin/styles.ts
+++ src/screens/signin/styles.ts
@@ -1,11 +1,52 @@
-import styled from 'styled-components/native';
+import styled, { css } from 'styled-components/native';
 import { LinearGradient } from 'expo-linear-gradient';
+import { getBottomSpace } from 'react-native-iphone-x-helper';
 
-export const Container = styled(LinearGradient).attrs(({ theme }) => ({
-  colors: theme.colors.gradient,
-  start: {x: 0, y: 1},
-  end: {x: 0.5, y: 0.5}
-}))`
+export const Container = styled(LinearGradient).attrs(({ theme }) => ({ colors: theme.colors.gradient, start: {x: 0, y: 1}, end: {x: 0.5, y: 0.5} }))`
   flex: 1;
   justify-content: center;
+`;
+
+export const Content = styled.ScrollView.attrs({
+  showsVerticalScrollIndicator: false,
+  contentContainerStyles: {
+    paddingBottom: getBottomSpace() + 48
+  }
+})`
+  width: 100%;
+  padding: 0 32px;
+`;
+
+export const Title = styled.Text`
+  margin-bottom: 24px;
+  align-self: flex-start;
+
+  ${({ theme }) => css`
+    font-size: ${theme.fonts.size.title};
+    font-family: ${theme.fonts.title};
+    color: ${theme.colors.title};
+  `};
+`;
+
+// melhorar esse brand
+export const Brand = styled.Image.attrs({
+  resizeMode: 'contain'
+})`
+  height: 250px;
+  margin-top: 64px;
+  margin-bottom: 32px;
+`;
+
+export const ForgotPasswordButton = styled.TouchableOpacity`
+  align-self: flex-end;
+  margin-bottom: 20px;
+`;
+
+export const ForgotPasswordLabel = styled.Text`
+  font-size: 14px;
+
+  ${({ theme }) => css`
+    font-family: ${theme.fonts.text};
+    color: ${theme.colors.title};
+  `};
 `;
